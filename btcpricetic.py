7dcbec6bcf4df6d93742075e351a1a2256c67dfc
Shane Davison <shanodaviso@gmail.com>
2020-12-15 11:39:55 +0100

    Deleted are nonsense now ready to code

--- btcpricetic.py
+++ btcpricetic.py
@@ -1,4 +1,27 @@
-Dom Smells like pee
-No Shane smells like pe
 
-nooow we can do some coding maybe
+
+import requests
+userinput = input('please type your cryto');
+usercurrency = input('please type your currency');
+
+
+# bitcoin_api_url = "https://api.coingecko.com/api/v3/simple/price?ids="+ str(userinput) + "&vs_currencies=usd"
+bitcoin_api_url = "https://api.coingecko.com/api/v3/simple/price?ids="+str(userinput)+ "&vs_currencies="+str(usercurrency)+""
+response = requests.get(bitcoin_api_url)
+bitcoinprice = response.json()
+
+bitcoinvalue = bitcoinprice[str(userinput)][str(usercurrency)]
+
+print(bitcoinvalue)
+
+# print (bitcoinprice)
+
+
+
+
+
+
+
+
+
+
