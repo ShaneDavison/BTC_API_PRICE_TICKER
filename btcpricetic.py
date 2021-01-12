c5b8f3935068996db35af556f6f91a794360cfde
Shane Davison <shanodaviso@gmail.com>
2021-01-12 11:00:01 +0100

    price coming in as float so ready to move forward with iftt

--- btcpricetic.py
+++ btcpricetic.py
@@ -1,20 +1,19 @@
-
-
 import requests
-userinput = input('please type your cryto');
-usercurrency = input('please type your currency');
+bitcoin_api_url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+
+def get_latest_bitcoin_price():
+    response = requests.get(bitcoin_api_url)
+    response_json  = response.json()
+    return (response_json['bitcoin']['usd'])
 
 
-# bitcoin_api_url = "https://api.coingecko.com/api/v3/simple/price?ids="+ str(userinput) + "&vs_currencies=usd"
-bitcoin_api_url = "https://api.coingecko.com/api/v3/simple/price?ids="+str(userinput)+ "&vs_currencies="+str(usercurrency)+""
-response = requests.get(bitcoin_api_url)
-bitcoinprice = response.json()
 
-bitcoinvalue = bitcoinprice[str(userinput)][str(usercurrency)]
 
-print(bitcoinvalue)
 
-# print (bitcoinprice)
+
+
+
+
 
 
 
