928cf719582a9f0a753333077b6023655a913342
Shane Davison <shanodaviso@gmail.com>
2021-01-12 11:01:14 +0100

    Forgot to add float line 7. Fixed now

--- btcpricetic.py
+++ btcpricetic.py
@@ -4,7 +4,7 @@ bitcoin_api_url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_
 def get_latest_bitcoin_price():
     response = requests.get(bitcoin_api_url)
     response_json  = response.json()
-    return (response_json['bitcoin']['usd'])
+    return float(response_json['bitcoin']['usd'])
 
 
 
